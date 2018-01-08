baed8e9d31a216dd845b48fd6c00f986c471d835
SeongIk <kpic1638@gmail.com>
2018-01-09 01:13:27 +0900

    I'm sorry

---
+++ 18.01.08/Palindrome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static bool isPalindrome(string word)
+    {
+        char[] wordR = word.ToCharArray();
+        Array.Reverse(wordR);
+        string wordRstr = new string(wordR);
+
+        return string.Equals(word, wordRstr, StringComparison.CurrentCultureIgnoreCase);
+
+        throw new NotImplementedException("Waiting to be implemented.");
+    }
+
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine(Palindrome.isPalindrome("Deleveled"));
+
+        }
+        catch (NotImplementedException)
+        {
+            Console.WriteLine("Not done yet");
+        }
+    }
+}
+
---
+++ 18.01.08/UserInput.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class TextInput
+{
+    protected string content = "";
+    public virtual void Add(char c)
+    {
+        content += c;
+    }
+    public string GetValue()
+    {
+        return content;
+    }
+    public TextInput() { }
+    public TextInput(string input)
+    {
+        content = input;
+    }
+}
+
+public class NumericInput : TextInput
+{
+    public override void Add(char c)
+    {
+        if (c >= 48 && c <= 57)
+        {
+            content += c;
+        }
+        else { };
+    }
+}
+
+
+public class UserInput
+{
+    public static void Main(string[] args)
+    {
+        TextInput input = new NumericInput();
+        input.Add('1');
+        input.Add('a');
+        input.Add('0');
+        Console.WriteLine(input.GetValue());
+    }
+}
